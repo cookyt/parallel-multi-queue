614bd81886f2ad98396db3cf9533e599a00ff6fc
Carlos Valera <cookyt2@gmail.com>
2013-03-13 02:18:08 -0400

    Added comment about usage of nextPow2

--- src/util.cc
+++ src/util.cc
@@ -25,6 +25,9 @@ bool cvl::atomic::cas64(uint64_t *addr, uint64_t oldval, uint64_t newval)
     return __sync_bool_compare_and_swap(addr, oldval, newval);
 }
 
+/**
+ * Calculates the next power of two larger than the input.
+ */
 int cvl::nextPow2(int n)
 {
     int cur = 1;
