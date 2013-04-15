6868d1290cb61531ff61e20b02ff22aa2a35d6d0
Carlos Valera <cookyt2@gmail.com>
2013-04-15 12:27:55 -0400

    Generated GNUplot scripts generate SVG files now

--- tests/scale.py
+++ tests/scale.py
@@ -44,6 +44,7 @@ if __name__ == "__main__":
         5. The max number of consumers
         6. The max number of producers
         7. The first line from the file (used as a title)
+        8. The name of the new scaled file
     """
     all_data = dict()
 
@@ -89,6 +90,8 @@ if __name__ == "__main__":
         all_data[fname].append(max_consumers+1)
         all_data[fname].append(max_producers+1)
         all_data[fname].append(lines[0].replace("#", "").strip()) # title
+        all_data[fname].append(newname)
+        str.replace
 
         f.close()
 
@@ -100,24 +103,43 @@ if __name__ == "__main__":
         min_rad2 = min_rad*min_rad
         return (((val-min_val)/d_val)*d_rad2 + min_rad2)**0.5
 
+    def scale_min0(val):
+        d_rad2 = d_rad*d_rad
+        min_rad2 = min_rad*min_rad
+        return ((val/max_val)*d_rad2 + min_rad2)**0.5
+
     def scale_lin(val):
         return (((val-min_val)/d_val)*d_rad + min_rad)
+
+    def getTitle(s):
+        if s.find("locking") != -1:
+            return "Two-Lock Queue Throughput (items/sec)"
+        elif s.find("multi") != -1:
+            return "MultiQueue Throughput (items/sec)"
+        else:
+            return s
+
+    def svgFname(s):
+        lst = s.rsplit("dat", 1)
+        name = "svg".join(lst)
+        return name[name.rfind('/') + 1:]
 
     for (fname, data) in all_data.iteritems():
         nf = data[0]
 
         nf.write("#!/usr/bin/env gnuplot\n")
         nf.write("# min=%f, max=%f\n"%(data[2], data[3]))
-        nf.write("set title '%s'\n"%data[6])
+        nf.write("set title '%s'\n"%getTitle(data[6]))
         nf.write("set size ratio -1\n")
         nf.write("set xrange [0:%d]\n"%data[4])
         nf.write("set yrange [0:%d]\n"%data[5])
-        nf.write("set xlabel 'consumers'\n")
-        nf.write("set ylabel 'producers'\n")
+        nf.write("set xlabel 'Consumer Threads'\n")
+        nf.write("set ylabel 'Producer Threads'\n")
+        nf.write("set term svg\n")
+        nf.write("set output '%s'\n"%svgFname(data[7]))
         nf.write("plot '-' with circles fill solid notitle\n")
         for point in data[1]:
-            nf.write("%d\t%d\t%f\n"%(point[0], point[1], scale(point[2])))
+            nf.write("%d\t%d\t%f\n"%(point[0], point[1], scale_min0(point[2])))
         nf.write("e\n")
-        nf.write("pause -1")
 
         nf.close()
